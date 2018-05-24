dcc98bfe9ac9ee83b1551aab159c5e615bb44972
CoolikV <VadimO.Kulik@livenau.net>
2018-05-24 22:40:52 +0300

    AutomapperConfiguration
    Added profiles for bll<->dal entities and
    web<->bll entities, configure global.asax

---
+++ BLL/Infrastructure/BLLMapperProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.DTO;
+using DAL.Entities;
+
+namespace BLL.Infrastructure
+{
+    public class BLLMapperProfile : Profile
+    {
+        public BLLMapperProfile()
+        {
+            CreateMap<Tour, TourDTO>();
+            CreateMap<Hotel, HotelDTO>();
+            CreateMap<Transport, TransportDTO>();
+            CreateMap<User, UserDTO>();
+
+            CreateMap<TourBooking, TourOrderDTO>();
+            CreateMap<HotelBooking, HotelOrderDTO>();
+            CreateMap<TransportBooking, TransportOrderDTO>();
+
+            CreateMap<HotelOrderDTO, HotelBooking>();
+            CreateMap<TourOrderDTO, TourBooking>();
+            CreateMap<TransportOrderDTO, TransportBooking>();
+        }
+    }
+}
---
+++ TourAgency/Util/WebMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using TourAgency.Models;
+using BLL.DTO;
+
+namespace TourAgency.Util
+{
+    public class WebMapperProfile : Profile
+    {
+        public WebMapperProfile()
+        {
+            CreateMap<TourViewModel, TourDTO>();
+            CreateMap<HotelViewModel, HotelDTO>();
+            CreateMap<TransportViewModel, TransportDTO>();
+            CreateMap<TourOrderViewModel, TourOrderDTO>();
+
+            CreateMap<HotelOrderViewModel, HotelOrderDTO>();
+
+            CreateMap<TransportOrderViewModel, TransportOrderDTO>();
+            CreateMap<HotelDTO, HotelViewModel>();
+            CreateMap<TourDTO, TourViewModel>();
+            CreateMap<TransportDTO, TransportViewModel>();
+        }
+    }
+}
